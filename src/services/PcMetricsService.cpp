c7ebd0bf379941ce3db82cc280d9239632198c3e
banecuk <banecuk@gmail.com>
2025-05-12 18:25:39 +0200

    parse additional data

--- src/services/PcMetricsService.cpp
+++ src/services/PcMetricsService.cpp
@@ -37,9 +37,45 @@ bool PcMetricsService::parseData(const String &rawData, PcMetrics &outData) {
     Serial.print("Memory usage: ");
     Serial.println(doc.memoryUsage());
 
-    // ...existing JSON parsing logic...
+    // Navigate through the JSON structure and extract data
     const JsonArrayConst &childrenPC = doc["Children"][0]["Children"];
-    // Parse other fields as before...
+
+    // Motherboard data
+    const JsonArrayConst &childrenMB = childrenPC[0]["Children"][0]["Children"];
+    outData.cpu_temperature = atof(childrenMB[1]["Children"][0]["Value"]);
+    outData.cpu_fan = atoi(childrenMB[2]["Children"][0]["Value"]);
+    outData.front_fan = atoi(childrenMB[2]["Children"][2]["Value"]);
+    outData.back_fan = atoi(childrenMB[2]["Children"][6]["Value"]);
+
+    // CPU data
+    const JsonArrayConst &childrenCPU = childrenPC[1]["Children"];
+    outData.cpu_power = static_cast<uint16_t>(atof(childrenCPU[1]["Children"][0]["Value"]) + 0.5);
+    outData.cpu_load = static_cast<uint8_t>(atof(childrenCPU[4]["Children"][0]["Value"]) + 0.5);
+
+    for (int i = 1; i < 21; i++) {
+        outData.cpu_thread_load[i - 1] = atof(childrenCPU[4]["Children"][i]["Value"]);
+    }
+
+    // Memory data
+    const JsonArrayConst &childrenRAM = childrenPC[2]["Children"][0]["Children"];
+    outData.mem_load = static_cast<uint8_t>(atof(childrenRAM[0]["Value"]) + 0.5);
+
+    // GPU data
+    const JsonArrayConst &childrenGPU = childrenPC[3]["Children"];
+    outData.gpu_power = static_cast<uint16_t>(atof(childrenGPU[1]["Children"][0]["Value"]) + 0.5);
+    outData.gpu_temperature = atof(childrenGPU[3]["Children"][0]["Value"]);
+    outData.gpu_load = static_cast<uint8_t>(atof(childrenGPU[4]["Children"][0]["Value"]) + 0.5);
+    outData.gpu_3d = static_cast<uint8_t>(atof(childrenGPU[4]["Children"][2]["Value"]) + 0.5);
+    outData.gpu_compute = static_cast<uint8_t>(atof(childrenGPU[4]["Children"][3]["Value"]) + 0.5);
+    outData.gpu_decode = static_cast<uint8_t>(atof(childrenGPU[4]["Children"][8]["Value"]) + 0.5);
+    outData.gpu_fan = atoi(childrenGPU[5]["Children"][0]["Value"]);
+    outData.gpu_mem = atof(childrenGPU[8]["Children"][0]["Value"]) / 163.84;
+
+    // Ethernet data
+    const JsonArrayConst &childrenETH = childrenPC[12]["Children"];
+    const JsonArrayConst &childrenETHThroughput = childrenETH[2]["Children"];
+    //outData.eth_up = this->parseNetworkSpeed(childrenETHThroughput[0]["Value"]);
+    //outData.eth_dn = this->parseNetworkSpeed(childrenETHThroughput[1]["Value"]);
 
     // Mark data as available and updated
     outData.is_available = true;
